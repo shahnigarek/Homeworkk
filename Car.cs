7ba832f45b2871b4c1dafff411fb0d031352b85c
Shahnigar E. Kazimli <shahnigarek@code.local>
2022-07-07 13:52:49 +0400

    bmw&&mercedes

---
+++ Car.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace homework2
+{
+   public  class Car
+    {
+          
+      
+        public string brand;
+        public int speed;
+        public int price;
+        public string color;
+
+       
+
+        
+
+
+    }
+}
+
+
+
+
---
+++ Program.cs
@@ -0,0 +1,33 @@
+﻿using System;
+
+namespace homework2
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            BMW bmw = new BMW();
+            bmw.brand = "BMW";
+            bmw.speed = 500;
+            bmw.price = 3500000;
+            bmw.color = "white";
+
+
+            Mercedes mercedes = new Mercedes();
+
+            mercedes.brand = "Mercedes";
+            mercedes.speed = 450;
+            mercedes.price = 3450000;
+            mercedes.color = "black";
+
+
+            Console.WriteLine(bmw.color);
+            Console.WriteLine(mercedes.speed);
+
+        }
+    }
+  
+
+   
+
+}
